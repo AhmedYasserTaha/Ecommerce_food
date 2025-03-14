b1680aa5766e1d9ce53bca2023f637a8155ae08b
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-14 13:48:43 +0200

    first commit & create splash screen

---
+++ lib/core/constes/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static Color Korang = Color(0xffFA4A0C);
+  static Color Kwhite = Color(0xffFFFFFF);
+  static Color Kgrey = Color(0xffF2F2F2);
+}
