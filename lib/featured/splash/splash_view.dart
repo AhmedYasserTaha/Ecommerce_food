27bd1e7a038f8eb60330088a1852dc0acdcd42ce
AhmedYasserTaha <ayt614614@gmail.com>
2025-03-14 14:05:25 +0200

    finshed splash view

--- lib/featured/splash/splash_view.dart
+++ lib/featured/splash/splash_view.dart
@@ -1,7 +1,29 @@
+import 'package:e_commerce/core/app_router/app_router.dart';
+import 'package:e_commerce/featured/home/home_view.dart';
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    Future.delayed(
+      Duration(seconds: 3),
+      () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomeView(),
+            ));
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
